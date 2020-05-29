eda2ed6f2bad2b50b3027ccf1275ecba5713ee9c
Matthias Berth <matthias.berth@gmail.com>
2020-05-29 13:05:54 +0200

    Update check_golden_master.sh
    ignore white space changes when diffing

--- check_golden_master.sh
+++ check_golden_master.sh
@@ -1,1 +1,1 @@
-diff -q output/classification/ golden_master/classification_project/classification | grep -v "Only in golden_master"
+diff -q --ignore-space-change output/classification/ golden_master/classification_project/classification | grep -v "Only in golden_master"
