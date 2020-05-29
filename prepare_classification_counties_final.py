3e6d80bd63022aaa3d16686df0a1b2dd4d3fbab6
Matthias Berth <matthias.berth@gmail.com>
2020-05-29 06:13:07 +0200

    fix directory

--- prepare_classification_counties_final.py
+++ prepare_classification_counties_final.py
@@ -179,7 +179,7 @@ for name in e_dataframe1.columns[3:]:
             #print(name,y3)
             ratio=0
             color="green"
-            with open(output_directory + '/' + str(ids[recs.index(name)]["UID"]) + '.json', 'w') as outfile:
+            with open(output_directory + '/classification/data_counties_' + str(ids[recs.index(name)]["UID"]) + '.json', 'w') as outfile:
                 json.dump({"dates":tim2,"max_14":int(max(y5)),"max":int(max(y)),"value":y3,"time":tim,"original_values":v},outfile)
             aar.append({"color":color,"province":name.split(",")[0],"country":name.split(",")[1],"id":"new_id_"+str(ind4),"value1":ratio, "dates":tim2,"value":y3})
             aar1.append({"n":name,"id":ids[recs.index(name)]["UID"],"v":ratio,"c":color,"max":int(max(y5))})
