09af95828fe4181d717af435fa5daba270155f52
obuchel <obuchel@gmail.com>
2020-06-07 09:23:15 -0400

    Updated 6/7/20

--- prepare_classification_counties_final.py
+++ prepare_classification_counties_final.py
@@ -5,7 +5,7 @@ import pandas as pd
 import os
 from prep_canada_data import stage_latest
 
-date_of_analysis='6/5/20'
+date_of_analysis='6/6/20'
 output_directory = 'output'
 os.makedirs(output_directory + '/classification', exist_ok=True)
 
