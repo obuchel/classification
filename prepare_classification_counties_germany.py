e0251ced383e6c396488a8970f302633950734e2
obuchel <obuchel@gmail.com>
2020-06-10 22:49:26 -0400

    Updated Germany

--- prepare_classification_counties_germany.py
+++ prepare_classification_counties_germany.py
@@ -296,7 +296,7 @@ for name in counties:
     x2 = x[9:]
     tim2 = tim[4 : -5]
     #print(pd.DataFrame(y1, columns=["a"]).rolling(window=7).mean()['a'].to_list())
-    y3 = pd.DataFrame(y1, columns=["a"]).rolling(window=7).mean()['a'].to_list()[6:]
+    y3 = pd.DataFrame(y1, columns=["a"]).rolling(window=3).mean()['a'].to_list()[6:]
     ys = y3[-24:]
     xs = x[-29:-5]  # last 24 days                                                                                                                                                   
     ind2 = 0
