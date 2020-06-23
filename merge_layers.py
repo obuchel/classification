6158e820d4818b2641a06c55a1e9c5bb87c43dff
obuchel <obuchel@gmail.com>
2020-06-23 00:15:38 -0400

    Updated Argentina

--- merge_layers.py
+++ merge_layers.py
@@ -1,32 +1,32 @@
-<<<<<<< HEAD
-import json
-data0={}
-with open("../ARG_adm4.json", "r") as read_file:
-=======
+
 
 import json
-data0={}
-with open("ARG_adm2.json", "r") as read_file:
->>>>>>> 78c18e17230951576ec3775f38adc6fd2dd422b8
+data0={"type":"FeatureCollection"}
+data0["features"]=[]
+data4={"type":"FeatureCollection"}
+data4["features"]=[]
+with open("ARG_adm3.json", "r") as read_file:
     data=json.load(read_file)
-    data0=data
-#'properties': {'ID_0': 12, 'ISO': 'ARG', 'NAME_0': 'Argentina', 'ID_1': 24, 'NAME_1': 'Tucumán', 'ID_2': 502, 'NAME_2': 'Trancas', 'TYPE_2': 'Departamento', 'ENGTYPE_2': 'Department', 'NL_NAME_2': '', 'VARNAME_2': ''}
+    #data0=data
+    for el in data["features"]:
+        if el["properties"]["NAME_2"]!="Mor\u00f3n":
+            data0["features"].append(el)
 
-<<<<<<< HEAD
-with open("communas.json", "r") as read_file:
-=======
-with open("converter3.geojson", "r") as read_file:
->>>>>>> 78c18e17230951576ec3775f38adc6fd2dd422b8
+with open("ARG3.geojson", "r") as read_file:
     data2=json.load(read_file)
     for el in data2["features"]:
-        #{'BARRIOS': 'BELGRANO - COLEGIALES - NUÑEZ', 'PERIMETRO': 26198.8269533, 'AREA': 14713213.1821, 'COMUNAS': 13.0, 'ID': 15, 'OBJETO': 'LIMITE COMUNAL'}
-        #print(el["properties"])
-        el["properties"]["NAME_1"]="Ciudad de Buenos Aires"
-        el["properties"]["NAME_2"]="COMUNA "+str(el["properties"]['COMUNAS']).split(".")[0]
-        el["properties"]["ID_2"]="com_"+str(el["properties"]["ID"])
-        print(el)
-        data0["features"].append(el)
-#print(data0)
+        #print(el)
+        if el["properties"]["sag"]=="ARBA - Gerencia de Servicios Catastrales" and el["properties"]["nam"] in ["Morón","Ituzaingó","Hurlingham"]:
+            el["properties"]["NAME_1"]="Buenos Aires"
+            el["properties"]["NAME_2"]=el["properties"]["nam"]
+            el["properties"]["ID_2"]="abr_"+str(["Morón","Ituzaingó","Hurlingham"].index(el["properties"]["nam"]))
+            data0["features"].append(el)
+print(data0)
 
-with open("ARG_adm3.json", "w") as read_file:
-    json.dump(data0,read_file)
+with open("ARG_adm3_1.json", "w") as write_file:
+    json.dump(data0,write_file)
+
+'''    
+with open("ARG_adm3_3.json", "w") as write_file:
+    json.dump(data4,write_file)
+'''
