6ce0c937a7efbeaa661aa27421a8267d2e36cd03
obuchel <obuchel@gmail.com>
2020-06-30 18:34:53 -0400

    Update Argentina

--- prepare_classification_counties_argentina.py
+++ prepare_classification_counties_argentina.py
@@ -16,12 +16,12 @@ import csv
 import unicodecsv
 import matplotlib.pyplot as plt
 onlyfiles = [f for f in listdir('/Users/olgabuchel/Downloads/2020-rki-archive-master/data/0_archived/') if isfile(join('/Users/olgabuchel/Downloads/2020-rki-archive-master/data/0_archived/', f))]
-date_of_analysis='6/29/20'
+date_of_analysis='6/30/20'
 
 output_directory = 'output_argentina'
 os.makedirs(output_directory + '/classification', exist_ok=True)
 
-url='../Covid19Casos.csv'
+url='../Covid19Casos.csv.2'
 all_data=[]
 kkeys=[]
 lists={}
