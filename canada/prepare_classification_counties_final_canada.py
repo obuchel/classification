02df29dd16d9dc1349f6981c23969be90d32d317
obuchel <obuchel@gmail.com>
2020-07-04 23:44:41 -0400

    Updated Canada

--- canada/prepare_classification_counties_final_canada.py
+++ canada/prepare_classification_counties_final_canada.py
@@ -194,12 +194,12 @@ for name in counties:
         min0 = np.min(ys)
         if max0 > 0:
             ratio = y3[-1] / max0
-            recent_mean = int(np.mean(original_values[-10:]))
-            #if recent_mean:
-            print(recent_mean,threshold,name,ratio)
-            color = classify(ratio, recent_mean, threshold)
-            #else:
-            #    color="green"
+            recent_mean = int(np.mean(original_values[-14:]))
+            if recent_mean>threshold:
+                print(recent_mean,threshold,name,ratio)
+                color = classify(ratio, recent_mean, threshold)
+            else:
+                color="green"
         else:
             #print(name,y3)
             ratio=0
