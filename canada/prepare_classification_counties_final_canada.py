0c089b47875c31278ce40270a666d8c06c671fa2
obuchel <obuchel@gmail.com>
2020-07-08 00:28:32 -0400

    Updated Canada

--- canada/prepare_classification_counties_final_canada.py
+++ canada/prepare_classification_counties_final_canada.py
@@ -7,7 +7,7 @@ import os
 from prep_canada_data import stage_latest
 
 
-date_of_analysis='7/6/20'
+date_of_analysis='7/7/20'
 
 output_directory = 'output_canada'
 os.makedirs(output_directory + '/classification', exist_ok=True)
