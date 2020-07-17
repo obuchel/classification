8820744e9860d574608577c730e0105fce99b224
obuchel <obuchel@gmail.com>
2020-07-16 22:11:21 -0400

    Updated Argentina

--- prepare_classification_counties_argentina.py
+++ prepare_classification_counties_argentina.py
@@ -14,12 +14,12 @@ import csv
 import unicodecsv
 import matplotlib.pyplot as plt
 onlyfiles = [f for f in listdir('/Users/olgabuchel/Downloads/2020-rki-archive-master/data/0_archived/') if isfile(join('/Users/olgabuchel/Downloads/2020-rki-archive-master/data/0_archived/', f))]
-date_of_analysis='7/14/20'
+date_of_analysis='7/16/20'
 
 output_directory = 'output_argentina'
 os.makedirs(output_directory + '/classification', exist_ok=True)
 
-url='/Users/olgabuchel/Downloads/Covid19Casos.csv'
+url='/Users/olgabuchel/Downloads/Covid19Casos (1).csv'
 all_data=[]
 kkeys=[]
 lists={}
