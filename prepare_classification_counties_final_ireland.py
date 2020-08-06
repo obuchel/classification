242f0c8f033826d5bff7c7e8008950eeb574a4ca
obuchel <obuchel@gmail.com>
2020-08-06 10:49:30 -0400

    Updated Ireland

--- prepare_classification_counties_final_ireland.py
+++ prepare_classification_counties_final_ireland.py
@@ -40,7 +40,7 @@ for x in range(4,9):
             dates.append(str(y)+"-"+str(x)+"-"+"2020")
 dates0=dates[:len(dates)-(31-int(date_of_analysis.split("/")[1]))]
 #print(dates0)
-data=pd.read_csv("/Users/olgabuchel/Downloads/Covid19CountyStatisticsHPSCIreland (4).csv")
+data=pd.read_csv("/Users/olgabuchel/Downloads/Covid19CountyStatisticsHPSCIreland (9).csv")
 print(data)
 data["date"]=[str(x).split(" ")[0] for x in data["TimeStamp"]]
 data["FID"]=data["CountyName"]
