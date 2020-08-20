ffaff448240913242d9fce01182e9868f50072ff
abuchel <abuchel.uwo.ca>
2020-08-19 21:59:03 -0400

    Updated Argentina. Arizona

--- prepare_classification_counties_argentina.py
+++ prepare_classification_counties_argentina.py
@@ -14,12 +14,12 @@ import csv
 import unicodecsv
 import matplotlib.pyplot as plt
 #onlyfiles = [f for f in listdir('/Users/olgabuchel/Downloads/2020-rki-archive-master/data/0_archived/') if isfile(join('/Users/olgabuchel/Downloads/2020-rki-archive-master/data/0_archived/', f))]
-date_of_analysis='8/17/20'
+date_of_analysis='8/19/20'
 
 output_directory = 'output_argentina'
 os.makedirs(output_directory + '/classification', exist_ok=True)
 
-url='/Users/olgabuchel/Downloads/Covid19Casos.csv'
+url='/home/abuchel/Documents/classification_project/Covid19Casos.csv'
 all_data=[]
 kkeys=[]
 lists={}
--- prepare_classification_counties_arizona.py
+++ prepare_classification_counties_arizona.py
@@ -1,16 +1,23 @@
-
 #https://data.mesaaz.gov/api/views/bcxg-q9nz/rows.csv?accessType=DOWNLOAD
 #https://data.mesaaz.gov/Fire-and-Medical/Daily-COVID-19-Cases-by-Zip-Code/bcxg-q9nz/data
+
 import json
+
 import numpy as np
 import pandas as pd
 import os
-date_of_analysis='8/9/20'
+#from prep_canada_data import stage_latest
+
+date_of_analysis='8/19/20'
+
+
 output_directory = 'output_arizona'
 os.makedirs(output_directory + '/classification', exist_ok=True)
+
 # Use canned CSV file, so we can compare results to earlier runs of the script.
 use_canned_file = False
-data = pd.read_csv('/Users/olgabuchel/Downloads/Daily_COVID-19_Cases_by_Zip_Code (2).csv')
+data = pd.read_csv('/home/abuchel/Downloads/Daily_COVID-19_Cases_by_Zip_Code.csv')
+#/abuchel/Downloads/Daily_COVID-19_Cases_by_Zip_Code.csv')
 data["dates"]=data.apply(lambda row: str(row.Date).split(" ")[0], axis=1)
 data["Combined_Key"]=data["Zip Code"]
 df=data#pd.pivot_table(data,index=["dates"],columns='Combined_Key',values=["Confirmed Cases Count"],aggfunc=np.sum)
