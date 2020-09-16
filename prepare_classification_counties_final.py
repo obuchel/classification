b0e5d697b4ab28d1a419f31a5024f9fa6d395480
abuchel <abuchel.uwo.ca>
2020-09-16 10:37:31 -0400

    Updated US

--- prepare_classification_counties_final.py
+++ prepare_classification_counties_final.py
@@ -6,7 +6,7 @@ import pandas as pd
 import os
 #from prep_canada_data import stage_latest
 #https://cdn.mbta.com/archive/archived_feeds.txt
-date_of_analysis='9/14/20'
+date_of_analysis='9/15/20'
 
 
 output_directory = 'output'
