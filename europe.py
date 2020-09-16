c4cfc86b2868e98cfb447fcfc37f378f38467961
abuchel <abuchel.uwo.ca>
2020-09-16 12:18:49 -0400

    Updated Europe

--- europe.py
+++ europe.py
@@ -234,7 +234,7 @@ for kkey in list(all_countries.keys()):
     print(counties)
     
     for name in counties:
-        values = e_dataframe0[name][80:].fillna(0)
+        values = e_dataframe0[name].fillna(0)
         print(values)
         num_rows = len(values)
         y50 = values[-14:]
@@ -276,7 +276,7 @@ for kkey in list(all_countries.keys()):
 
             print(name,color,ratio,recent_mean0,int(max(y5)))    
             with open(output_directory + '/classification/data_counties_'+str(ids[recs.index(name)]["Combined_Key"]).split(", ")[0]+'.json', 'w') as outfile:
-                json.dump({"dates":tim2[80:],"max_14": int(max(y5)-min(y5)),"max":int(max(y)),"value":y3,"time":tim[80:],"original_values":original_values},outfile)
+                json.dump({"dates":tim2,"max_14": int(max(y5)-min(y5)),"max":int(max(y)),"value":y3,"time":tim,"original_values":original_values},outfile)
             #aar.append({"color":color,"province":name.split(",")[0],"country":name.split(",")[1],"id":"new_id_"+str(ind4),"value1":ratio, "dates":tim2,"value":y3})
             aar1.append({"n":name.split(", ")[0],"id":ids[recs.index(name)]["Combined_Key"].split(", ")[0],"v":ratio,"c":color,"max":int(max(y5)-min(y5))})
             ind4+=1
