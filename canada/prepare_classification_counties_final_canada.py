7477590eb6695d2c9f3d4846922316335d51e63c
abuchel <abuchel.uwo.ca>
2020-09-15 21:31:49 -0400

    Updated Canada, Argentina

--- canada/prepare_classification_counties_final_canada.py
+++ canada/prepare_classification_counties_final_canada.py
@@ -9,7 +9,7 @@ import os
 from prep_canada_data import stage_latest
 
 
-date_of_analysis='9/14/20'
+date_of_analysis='9/15/20'
 
 output_directory = 'output_canada'
 os.makedirs(output_directory + '/classification', exist_ok=True)
