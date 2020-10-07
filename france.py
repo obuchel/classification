0c3f5cc6e81b91a3a89e6adc63de9ec5e132e60b
obuchel <obuchel@gmail.com>
2020-10-06 23:32:10 -0400

    Updated Europe

--- france.py
+++ france.py
@@ -12,7 +12,7 @@ import pandas as pd
 import os
 #from prep_canada_data import stage_latest
 #https://cdn.mbta.com/archive/archived_feeds.txt
-date_of_analysis='9/30/20'
+date_of_analysis='10/3/20'
 
 
 output_directory = 'output_france'
