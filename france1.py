cfbf5c90c508c3fbe58e23cd60e11d4f85f4142f
obuchel <obuchel@gmail.com>
2020-10-08 20:09:25 -0400

    Updated Germany, Belgium, France

--- france1.py
+++ france1.py
@@ -12,7 +12,7 @@ import pandas as pd
 import os
 #from prep_canada_data import stage_latest
 #https://cdn.mbta.com/archive/archived_feeds.txt
-date_of_analysis='10/6/20'
+date_of_analysis='10/5/20'
 
 
 output_directory = 'output1_france'
--- prepare_classification_counties_germany_new.py
+++ prepare_classification_counties_germany_new.py
@@ -12,7 +12,7 @@ import json
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-date_of_analysis='10/2/20'
+date_of_analysis='10/8/20'
 
 output_directory = 'output_germany'
 os.makedirs(output_directory + '/classification', exist_ok=True)
