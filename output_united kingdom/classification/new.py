bab689f539553e36b02a85fd4f65a5d0f7a53290
obuchel <obuchel@gmail.com>
2020-10-10 00:01:46 -0400

    Updated Argentina, Canada

---
+++ output_united kingdom/classification/new.py
@@ -0,0 +1,91 @@
+ampshire"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="kent"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="Kent"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="Oxfordshire,"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="Oxfordshire"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="berkshire"'
+$ 
+$ 
+$ -each 'NAME_2 = "South East"' where='NAME_2=="Berkshire"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="Surrey"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="West Sussex"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="Isle of Wight"'
+$ 
+$ -each 'NAME_2 = "Shetland"' where='NAME_2=="Shetland Islands"'
+$ -each 'NAME_2 = "Northern Ireland"' where='NAME_1=="Northern Ireland"'
+$  Northumberland, County Durham, Tyne and Wear and the former county of Cleveland
+The -i command cannot be run in the browser
+$ North East and Yorkshire
+The -i command cannot be run in the browser
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="Northumberland"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="County Durham"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="Tyne and Wear"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="North Yorkshire"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="East Riding of Yorkshire"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="York"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="Redcar and Cleveland"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="Stockton"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="Darlington"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="Middlesbrough"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="Hartlepool"'
+$ -each 'NAME_2 = "Orkney"' where='NAME_2=="Orkney Islands"'
+$  Derbyshire
+ Herefordshire
+ Leicestershire
+ Lincolnshire
+ Northamptonshire
+ Nottinghamshire
+ Rutland
+ Shropshire
+ Staffordshire
+ Warwickshire
+ Worcestershire
+The -i command cannot be run in the browser
+$ 
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="West Yorkshire"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="South Yorkshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Derbyshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Herefordshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Leicestershire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Lincolnshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Northamptonshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Nottinghamshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Rutland"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Shropshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Staffordshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Warwickshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Worcestershire"'
+$ -each 'NAME_2 = "Tayside"' where='NAME_2=="Perthshire and Kinross" || NAME_2=="Angus"'
+$ -each 'NAME_2 = "Lanarkshire"' where='NAME_2=="South Lanarkshire"'
+$ -each 'NAME_2 = "Lothian"' where='NAME_2=="East Lothian"'
+$ -each 'NAME_2 = "Lothian"' where='NAME_2=="Midlothian"'
+$ -each 'NAME_2 = "Lothian"' where='NAME_2=="West Lothian"'
+$ -each 'NAME_2 = "Lothian"' where='NAME_2=="Edinburgh"'
+$ -each 'NAME_2 = "Greater Glasgow and Clyde"' where='NAME_2=="West Dunbartonshire"'
+$ -each 'NAME_2 = "Greater Glasgow and Clyde"' where='NAME_2=="Inverclyde"'
+$ -each 'NAME_2 = "Greater Glasgow and Clyde"' where='NAME_2=="Renfrewshire"'
+$ -each 'NAME_2 = "Greater Glasgow and Clyde"' where='NAME_2=="East Renfrewshire"'
+$ -each 'NAME_2 = "Greater Glasgow and Clyde"' where='NAME_2=="East Dunbartonshire"'
+$ -each 'NAME_2 = "Greater Glasgow and Clyde"' where='NAME_2=="Glasgow"'
+$ -each 'NAME_2 = "Grampian"' where='NAME_1=="Aberdeenshire"'
+$ -each 'NAME_2 = "Grampian"' where='NAME_2=="Aberdeenshire"'
+$ -each 'NAME_2 = "Grampian"' where='NAME_2=="Moray"'
+$ -each 'NAME_2 = "Grampian"' where='NAME_2=="Aberdeen"'
+$ -each 'NAME_2 = "London"' where='TYPE_2=="London Borough"'
+$ -each 'NAME_2 = "London"' where='TYPE_2=="London Borough (royal)"'
+$ -each 'NAME_2 = "Forth Valley"' where='NAME_2=="Stirling"'
+$ 
+$ -each 'NAME_2 = "Forth Valley"' where='NAME_2=="Falkirk"'
+$ -each 'NAME_2 = "Forth Valley"' where='NAME_2=="Clackmannanshire"'
+$ -each 'NAME_2 = "Ayrshire and Arran"' where='NAME_2=="East Ayrshire" || NAME_2=="North Ayshire" || NAME_2=="South Ayrshire"'
+$ -each 'NAME_1 = "East of England"' where='NAME_2=="East of England"'
+$ -each 'NAME_2 = "Borders"' where='NAME_2=="Scottish Borders"'
+$ -each 'NAME_2 = "Eileanan Siar (Western Isles)"' where='NAME_2=="Eilean Siar"'
+$ -each 'NAME_2 = "East of England"' where='NAME_2=="Cambridgeshire"'
+$ -each 'NAME_2 = "East of England"' where='NAME_2=="Bedfordshire"'
+$ -each 'NAME_2 = "East of England"' where='NAME_2=="Hertfordshire"'
+$ -each 'NAME_2 = "East of England"' where='NAME_2=="Norfolk"'
+$ -each 'NAME_2 = "East of England"' where='NAME_2=="Suffolk"'
+$ -each 'NAME_2 = "East of England"' where='NAME_2=="Essex"'
+$ 
+$ -each 'NAME_1 = "London"' where='NAME_2=="London"'
---
+++ output_united kingdom/classification/new1.py
@@ -0,0 +1,161 @@
+ampshire"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="kent"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="Kent"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="Oxfordshire,"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="Oxfordshire"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="berkshire"'
+$ 
+$ 
+$ -each 'NAME_2 = "South East"' where='NAME_2=="Berkshire"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="Surrey"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="West Sussex"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="Isle of Wight"'
+$ 
+$ -each 'NAME_2 = "Shetland"' where='NAME_2=="Shetland Islands"'
+$ -each 'NAME_2 = "Northern Ireland"' where='NAME_1=="Northern Ireland"'
+$  Northumberland, County Durham, Tyne and Wear and the former county of Cleveland
+The -i command cannot be run in the browser
+$ North East and Yorkshire
+The -i command cannot be run in the browser
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="Northumberland"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="County Durham"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="Tyne and Wear"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="North Yorkshire"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="East Riding of Yorkshire"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="York"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="Redcar and Cleveland"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="Stockton"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="Darlington"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="Middlesbrough"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="Hartlepool"'
+$ -each 'NAME_2 = "Orkney"' where='NAME_2=="Orkney Islands"'
+$  Derbyshire
+ Herefordshire
+ Leicestershire
+ Lincolnshire
+ Northamptonshire
+ Nottinghamshire
+ Rutland
+ Shropshire
+ Staffordshire
+ Warwickshire
+ Worcestershire
+The -i command cannot be run in the browser
+$ 
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="West Yorkshire"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="South Yorkshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Derbyshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Herefordshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Leicestershire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Lincolnshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Northamptonshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Nottinghamshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Rutland"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Shropshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Staffordshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Warwickshire"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Worcestershire"'
+$ -each 'NAME_2 = "Tayside"' where='NAME_2=="Perthshire and Kinross" || NAME_2=="Angus"'
+$ -each 'NAME_2 = "Lanarkshire"' where='NAME_2=="South Lanarkshire"'
+$ -each 'NAME_2 = "Lothian"' where='NAME_2=="East Lothian"'
+$ -each 'NAME_2 = "Lothian"' where='NAME_2=="Midlothian"'
+$ -each 'NAME_2 = "Lothian"' where='NAME_2=="West Lothian"'
+$ -each 'NAME_2 = "Lothian"' where='NAME_2=="Edinburgh"'
+$ -each 'NAME_2 = "Greater Glasgow and Clyde"' where='NAME_2=="West Dunbartonshire"'
+$ -each 'NAME_2 = "Greater Glasgow and Clyde"' where='NAME_2=="Inverclyde"'
+$ -each 'NAME_2 = "Greater Glasgow and Clyde"' where='NAME_2=="Renfrewshire"'
+$ -each 'NAME_2 = "Greater Glasgow and Clyde"' where='NAME_2=="East Renfrewshire"'
+$ -each 'NAME_2 = "Greater Glasgow and Clyde"' where='NAME_2=="East Dunbartonshire"'
+$ -each 'NAME_2 = "Greater Glasgow and Clyde"' where='NAME_2=="Glasgow"'
+$ -each 'NAME_2 = "Grampian"' where='NAME_1=="Aberdeenshire"'
+$ -each 'NAME_2 = "Grampian"' where='NAME_2=="Aberdeenshire"'
+$ -each 'NAME_2 = "Grampian"' where='NAME_2=="Moray"'
+$ -each 'NAME_2 = "Grampian"' where='NAME_2=="Aberdeen"'
+$ -each 'NAME_2 = "London"' where='TYPE_2=="London Borough"'
+$ -each 'NAME_2 = "London"' where='TYPE_2=="London Borough (royal)"'
+$ -each 'NAME_2 = "Forth Valley"' where='NAME_2=="Stirling"'
+$ 
+$ -each 'NAME_2 = "Forth Valley"' where='NAME_2=="Falkirk"'
+$ -each 'NAME_2 = "Forth Valley"' where='NAME_2=="Clackmannanshire"'
+$ -each 'NAME_2 = "Ayrshire and Arran"' where='NAME_2=="East Ayrshire" || NAME_2=="North Ayshire" || NAME_2=="South Ayrshire"'
+$ -each 'NAME_1 = "East of England"' where='NAME_2=="East of England"'
+$ -each 'NAME_2 = "Borders"' where='NAME_2=="Scottish Borders"'
+$ -each 'NAME_2 = "Eileanan Siar (Western Isles)"' where='NAME_2=="Eilean Siar"'
+$ -each 'NAME_2 = "East of England"' where='NAME_2=="Cambridgeshire"'
+$ -each 'NAME_2 = "East of England"' where='NAME_2=="Bedfordshire"'
+$ -each 'NAME_2 = "East of England"' where='NAME_2=="Hertfordshire"'
+$ -each 'NAME_2 = "East of England"' where='NAME_2=="Norfolk"'
+$ -each 'NAME_2 = "East of England"' where='NAME_2=="Suffolk"'
+$ -each 'NAME_2 = "East of England"' where='NAME_2=="Essex"'
+$ 
+$ -each 'NAME_1 = "London"' where='NAME_2=="London"'
+$ -dissolve NAME_2
+[dissolve] Dissolved 192 features into 62 features
+$ -each 'NAME_2 = "South West"' where='NAME_2=="North Somerset"'
+$ 
+$ -each 'NAME_2 = "South West"' where='NAME_2=="Bath and North East Somerset"'
+$ -each 'NAME_2 = "South West"' where='NAME_2=="South Gloucestershire"'
+$ -dissolve NAME_2
+[dissolve] Dissolved 62 features into 59 features
+$ -each 'NAME_1 = "London"' where='NAME_2=="London"'
+$ -each 'NAME_2 = "London"' where='NAME_2=="Westminster"'
+$ -dissolve NAME_2
+[dissolve] Dissolved 59 features into 58 features
+$ 
+$ -each 'NAME_2 = "East of England"' where='NAME_2=="Luton"'
+$ -dissolve NAME_2
+[dissolve] Dissolved 58 features into 57 features
+$ 
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Peterborough"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Leicester"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="West Midlands"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Telford and Wrekin"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Stoke-on-Trent"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Midlands"'
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Derby"'
+$ 
+$ -each 'NAME_2 = "Midlands"' where='NAME_2=="Nottingham"'
+$ -dissolve NAME_2
+[dissolve] Dissolved 57 features into 50 features
+$ -each 'NAME_2 = "East of England"' where='NAME_2=="Milton Keynes"'
+$ -dissolve NAME_2
+[dissolve] Dissolved 50 features into 49 features
+$ -each 'NAME_2 = "East of England"' where='NAME_2=="Thurrock"'
+$ -dissolve NAME_2
+[dissolve] Dissolved 49 features into 48 features
+$ -each 'NAME_2 = "South East"' where='NAME_2=="Medway"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="Portsmouth"'
+$ -each 'NAME_2 = "South East"' where='NAME_2=="Southampton"'
+$ -dissolve NAME_2
+[dissolve] Dissolved 48 features into 45 features
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="Durham"'
+$ -each 'NAME_2 = "North East and Yorkshire"' where='NAME_2=="Stockton-on-Tees"'
+$ -dissolve NAME_2
+[dissolve] Dissolved 45 features into 43 features
+$ Cheshire, Cumbria, Greater Manchester, Lancashire and Merseyside
+The -i command cannot be run in the browser
+$ -each 'NAME_2 = "North West"' where='NAME_2=="Cheshire"'
+$ -each 'NAME_2 = "North West"' where='NAME_2=="Cumbria"'
+$ -each 'NAME_2 = "North West"' where='NAME_2=="Lancashire"'
+$ -each 'NAME_2 = "North West"' where='NAME_2=="Merseyside"'
+$ -each 'NAME_2 = "North West"' where='NAME_2=="Manchester"'
+$ -dissolve NAME_2
+[dissolve] Dissolved 43 features into 39 features
+$ -each 'NAME_2 = "North West"' where='NAME_2=="Warrington"'
+$ -each 'NAME_2 = "North West"' where='NAME_2=="Blackburn with Darwen"'
+$ 
+$ -dissolve NAME_2
+[dissolve] Dissolved 39 features into 37 features
+$ 
+$ 
+$ -each 'NAME_2 = "North West"' where='NAME_2=="Halton"'
+$ -dissolve NAME_2
+[dissolve] Dissolved 37 features into 36 features
+$ -each 'NAME_2 = "Lanarkshire"' where='NAME_2=="North Lanarkshire"'
+$ -dissolve NAME_2
+[dissolve] Dissolved 36 features into 35 features
+$ ~
+
+
+
+
