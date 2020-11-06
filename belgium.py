9e4d38b05537913e753951c0bbc8944b68fbd981
obuchel <obuchel@gmail.com>
2020-11-06 09:34:28 -0500

    Updated Europe, Canada, US

--- belgium.py
+++ belgium.py
@@ -1,5 +1,6 @@
 
 
+
 #https://static.data.gouv.fr/resources/donnees-relatives-aux-resultats-des-tests-virologiques-covid-19/20200829-191505/sp-pos-quot-dep-2020-08-29-19h15.csv
 #https://www.data.gouv.fr/fr/datasets/r/406c6a23-e283-4300-9484-54e78c8ae675
 #https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/departements-avec-outre-mer.geojson
@@ -21,7 +22,7 @@ import pandas as pd
 import os
 #from prep_canada_data import stage_latest
 #https://cdn.mbta.com/archive/archived_feeds.txt
-date_of_analysis='10/31/20'
+date_of_analysis='11/6/20'
 
 df = pd.read_excel(r'https://epistat.sciensano.be/Data/COVID19BE.xlsx', sheet_name='CASES_AGESEX')
 df3=df.dropna().groupby(["DATE","PROVINCE","REGION"])["CASES"].sum().reset_index()
--- prepare_classification_counties_sweden.py
+++ prepare_classification_counties_sweden.py
@@ -1,4 +1,5 @@
 
+
 #https://www.arcgis.com/sharing/rest/content/items/b5e7488e117749c19881cce45db13f7e/data
 #https://data.mesaaz.gov/Fire-and-Medical/Daily-COVID-19-Cases-by-Zip-Code/bcxg-q9nz/data
 import json
@@ -8,9 +9,9 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 #from prep_canada_data import stage_latest
-date_of_analysis='10/29/20'
+date_of_analysis='11/6/20'
 #xls = pd.ExcelFile('/home/abuchel/Downloads/Folkhalsomyndigheten_Covid19.xlsx')
-df = pd.read_excel(r'/Users/olgabuchel/Downloads/Folkhalsomyndigheten_Covid19 (11).xlsx', sheet_name='Antal per dag region')
+df = pd.read_excel(r'/Users/olgabuchel/Downloads/Folkhalsomyndigheten_Covid19.xlsx', sheet_name='Antal per dag region')
 print(df)
 df["dates"]=[str(l).split("T")[0] for l in list(df.Statistikdatum.unique())]
 output_directory = 'output_sweden'
