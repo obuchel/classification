1a225299027a99e8a2ff95dd139096d919216613
obuchel <obuchel@gmail.com>
2020-11-30 23:25:05 -0500

    Updated Argentina, Canada

--- canada/prepare_classification_counties_final_canada.py
+++ canada/prepare_classification_counties_final_canada.py
@@ -6,7 +6,7 @@ import os
 from prep_canada_data import stage_latest
 
 
-date_of_analysis='11/29/20'
+date_of_analysis='11/30/20'
 
 output_directory = 'output_canada'
 os.makedirs(output_directory + '/classification', exist_ok=True)
--- prepare_classification_counties_final.py
+++ prepare_classification_counties_final.py
@@ -9,7 +9,7 @@ import pandas as pd
 import os
 #from prep_canada_data import stage_latest
 #https://cdn.mbta.com/archive/archived_feeds.txt
-date_of_analysis='11/28/20'
+date_of_analysis='11/29/20'
 
 
 output_directory = 'output'
