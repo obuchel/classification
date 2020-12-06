c3fb7a4fb5baab28bf2e3050d5f4e976156c827b
obuchel <obuchel@gmail.com>
2020-12-05 22:51:40 -0500

    Updated Ireland

--- prepare_classification_counties_final_ireland.py
+++ prepare_classification_counties_final_ireland.py
@@ -8,7 +8,7 @@ import numpy as np
 import os
 from datetime import datetime
 import matplotlib.pyplot as plt
-date_of_analysis='10/30/20'
+date_of_analysis='12/03/20'
 import math
 output_directory = 'output_ireland'
 os.makedirs(output_directory + '/classification', exist_ok=True)
@@ -44,7 +44,7 @@ for x in range(4,13):
             dates.append(str(y)+"-"+str(x)+"-"+"2020")
 dates0=dates[:len(dates)-(31-int(date_of_analysis.split("/")[1]))]
 #print(dates0)
-data=pd.read_csv("/Users/olgabuchel/Downloads/Covid19CountyStatisticsHPSCIreland (1).csv")
+data=pd.read_csv("/Users/olgabuchel/Downloads/Covid19CountyStatisticsHPSCIreland (2).csv")
 print(data)
 data["date"]=[str(x).split(" ")[0] for x in data["TimeStamp"]]
 data["FID"]=data["CountyName"]
