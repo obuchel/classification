e47980f6808fd404d225dee7c49185365200468f
obuchel <obuchel@gmail.com>
2020-12-10 11:03:29 -0500

    Updated Europe, Arizona, Ireland

--- prepare_classification_counties_arizona.py
+++ prepare_classification_counties_arizona.py
@@ -11,7 +11,7 @@ import pandas as pd
 import os
 #from prep_canada_data import stage_latest
 
-date_of_analysis='12/5/20'
+date_of_analysis='12/9/20'
 
 
 output_directory = 'output_arizona'
@@ -19,7 +19,7 @@ os.makedirs(output_directory + '/classification', exist_ok=True)
 
 # Use canned CSV file, so we can compare results to earlier runs of the script.
 use_canned_file = False
-data = pd.read_csv('/Users/olgabuchel/Downloads/Daily_COVID-19_Cases_by_Zip_Code.csv')
+data = pd.read_csv('/Users/olgabuchel/Downloads/Daily_COVID-19_Cases_by_Zip_Code (1).csv')
 #/abuchel/Downloads/Daily_COVID-19_Cases_by_Zip_Code.csv')
 data["dates"]=data.apply(lambda row: str(row.Date).split(" ")[0], axis=1)
 data["Combined_Key"]=data["Zip Code"]
