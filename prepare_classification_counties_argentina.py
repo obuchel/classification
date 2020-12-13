ca532bb97acae480aa8a7630b9fcf7484d3dd089
obuchel <obuchel@gmail.com>
2020-12-13 11:47:27 -0500

    Updated Argentina, Canada, US

--- prepare_classification_counties_argentina.py
+++ prepare_classification_counties_argentina.py
@@ -1,4 +1,5 @@
 
+
 #https://sisa.msal.gov.ar/datos/descargas/covid-19/files/Covid19Casos.csv
 import numpy as np
 import urllib.request as urllib2
@@ -13,7 +14,7 @@ import csv
 import unicodecsv
 import matplotlib.pyplot as plt
 #onlyfiles = [f for f in listdir('/Users/olgabuchel/Downloads/2020-rki-archive-master/data/0_archived/') if isfile(join('/Users/olgabuchel/Downloads/2020-rki-archive-master/data/0_archived/', f))]
-date_of_analysis='12/11/20'
+date_of_analysis='12/12/20'
 import wget
 
 url = 'https://sisa.msal.gov.ar/datos/descargas/covid-19/files/Covid19Casos.csv'
