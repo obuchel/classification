e4231bafa2be22fc57c9e0b932e709a6080b5b1b
obuchel <obuchel@gmail.com>
2020-12-19 23:15:06 -0500

    Updated Israel

--- prepare_israel_data.py
+++ prepare_israel_data.py
@@ -1,4 +1,4 @@
-#curl -d '%7B%22resource_id%22%3A%228a21d39d-91e3-40db-aca1-f73f7ab1df69%22%2C%22q%22%3A%22%22%2C%22filters%22%3A%7B%7D%2C%22include_total%22%3Atrue%2C%22limit%22%3A76167%2C%22offset%22%3A0%7D' https://data.gov.il/api/3/action/datastore_search -o israel_data_today.json
+#curl -d '%7B%22resource_id%22%3A%228a21d39d-91e3-40db-aca1-f73f7ab1df69%22%2C%22q%22%3A%22%22%2C%22filters%22%3A%7B%7D%2C%22include_total%22%3Atrue%2C%22limit%22%3A78120%2C%22offset%22%3A0%7D' https://data.gov.il/api/3/action/datastore_search -o israel_data_today.json
 
 import json
 import csv
