cb4e72b3b5dd87a8b2fb3b51e8102d2de5e0c4cd
obuchel <obuchel@gmail.com>
2021-02-21 19:39:28 -0500

    Updated Europe, Argentina, Canada, US

--- europe1.py
+++ europe1.py
@@ -25,7 +25,7 @@ import pandas as pd
 import os
 #from prep_canada_data import stage_latest
 #https://cdn.mbta.com/archive/archived_feeds.txt
-date_of_analysis='2/17/21'
+date_of_analysis='2/21/21'
 
 df = pd.read_csv('https://raw.githubusercontent.com/ec-jrc/COVID-19/master/data-by-region/jrc-covid-19-all-days-by-regions.csv')
 
