13a9459a8001c05de90f19e579ad0d184a5d5530
obuchel <obuchel@gmail.com>
2021-03-09 23:55:37 -0500

    Updated Canada

--- canada/prepare_classification_counties_final_canada.py
+++ canada/prepare_classification_counties_final_canada.py
@@ -4,10 +4,10 @@ import pandas as pd
 import os
 from prep_canada_data import stage_latest
 from datetime import date
-#date_of_analysis=date.today().strftime("%m/%d/%y")
-#print(date_of_analysis)
+date_of_analysis=date.today().strftime("%m/%d/%y")
+print(date_of_analysis)
 
-date_of_analysis='03/08/21'
+#date_of_analysis='03/08/21'
 
 output_directory = 'output_canada'
 os.makedirs(output_directory + '/classification', exist_ok=True)
